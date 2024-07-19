843e750fc4c83b39a9fb3fa68806c0840ddd5089
Leo <ucantseeit1000@gmail.com>
2024-07-19 09:30:35 +0800

    fix eval.cpp

--- eval.cpp
+++ eval.cpp
@@ -22,45 +22,22 @@ SyntaxTree eval_expr(const SyntaxTree & expr, Frame & env) {
         }
     }
     
-    Symbol sym;   SyntaxTree findSymResult;  
-    Proc op = nullptr;
+    // handle special forms
+    Symbol sym;  
     if (expr.items.front().isSymbol()) {
         sym = get<Symbol>(expr.items.front().value);
-    } else if (expr.items.front().isSubexpr()) {
-        auto item = eval_expr(expr.items.front(), env);
-        if (item.isSymbol()) {
-            sym = get<Symbol>(item.value);
-        }  else if (item.isProc()) {
-            op = get<Proc>(item.value);
-        } else {
-            throw std::invalid_argument(
-                "the first item in the parentheses is not a procedure");
+        if (specialForms.find(sym) != specialForms.end()) {
+            return handleSpecialForms(expr.items, env);
         }
-    } else {
-        throw std::invalid_argument(
-            "the first item in the parentheses is not a procedure");
     }
     
-
-
-
-    // handle special forms
-    if (!sym.empty() && specialForms.find(sym) != specialForms.end()) {
-        return handleSpecialForms(expr.items, env);
-    }
-
-    if (!sym.empty() && env.lookup(sym) == env.endOfFrame()) {
-        throw std::invalid_argument(
-            sym + " is undefined.");
-    } else if (!sym.empty()) {
-        findSymResult = env.lookup(sym)->second;
-    }
+    
+    SyntaxTree firstItem = eval_expr(expr.items.front(), env);
 
     // handle the built-in procedures
-    if (findSymResult.isProc()) {
-        op = get<Proc>(findSymResult.value);
-    }
-    if (op != nullptr) {
+    if (firstItem.isProc()) {
+        Proc op = get<Proc>(firstItem.value);
+
         list<SyntaxTree> arguments;
         auto track = std::next(expr.items.begin());
         for (; track != expr.items.end();
@@ -76,9 +53,9 @@ SyntaxTree eval_expr(const SyntaxTree & expr, Frame & env) {
     } 
     
     // handle the user-defined functions
-    if (findSymResult.isLambda()) {
+    if (firstItem.isLambda()) {
         // create a new frame to evaluate the call
-        Lambda lamb = get<Lambda>(findSymResult.value);
+        Lambda lamb = get<Lambda>(firstItem.value);
         Frame newFrame(lamb.pLocalFrame);
 
         // get the arguments of the call
@@ -109,7 +86,9 @@ SyntaxTree eval_expr(const SyntaxTree & expr, Frame & env) {
         return eval_sequence(lamb.body, newFrame);
     }
 
-    return nil;
+    throw std::invalid_argument(
+        "the first item in the parentheses is not a procedure\
+        or user-defined function");
 }
 
 SyntaxTree eval_sequence(const list<SyntaxTree> & exprs, Frame & env) {
